8d23d41e9b46f792bec49bef31468e905ea1551c
FengQing123 <1684908702@qq.com>
2020-01-03 10:43:08 +0800

    添加RecycleView滑动冲突解决方法

--- app/src/main/java/com/example/template/module/recycleview/RecycleViewTestActivity.java
+++ app/src/main/java/com/example/template/module/recycleview/RecycleViewTestActivity.java
@@ -2,6 +2,7 @@ package com.example.template.module.recycleview;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 
 import androidx.annotation.Nullable;
@@ -114,6 +115,40 @@ public class RecycleViewTestActivity extends BaseActivity {
          * 设置Adapter
          */
         mRecycleView.setAdapter(adapter);
+
+        /**
+         * 用于解决滑动冲突问题
+         * 例如：RecycleView嵌套在DrawableLayout里，左滑RecycleView无法滑动，DrawableLayout却被左滑了
+         *
+         * 原因是：RecycleView在处理水平滚动时，被DrawableLayout拦截了（先消费掉了)，
+         * 解决方法：我们可以在RecycleView收到Action_MOVE时，请求父布局不要拦截它的事件
+         * 具体解决方法：
+         * 1.给RecycleView设置一个OnTouchListener()方法
+         * 2.Action_MOVE里，调用它的getParent方法返回的ViewParent对象的requestDisallowInterceptTouchEvent方法，参数传true,表示不允许拦截事件
+         * 3.在ACTION_UP 和 ACTION_CANCEL里，调用requestDisallowInterceptTouchEvent方法，参数传false,无需处理滑动事件
+         * 4.最后,onTouch方法一定要返回false(用来告诉RecycleView,它要继续处理触摸事件)
+         *
+         * 但是RecycleView开始滚动，那么DrawableLayout就无法关闭了
+         * 用户想要的是：当列表滚动到最右边，手指继续向左滑动的时候，DrawableLayout就要响应事件
+         * 我们可以使用View的canScrollHorizontally方法来判断，它的参数是int值，正数检测是否能向右滚动，负数检测是否能向左滚动
+         *
+         */
+        mRecycleView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int action = event.getAction();
+                switch (action) {
+                    case MotionEvent.ACTION_MOVE:
+                        v.getParent().requestDisallowInterceptTouchEvent(v.canScrollHorizontally(1));
+                        break;
+                    case MotionEvent.ACTION_UP:
+                    case MotionEvent.ACTION_CANCEL:
+                        v.getParent().requestDisallowInterceptTouchEvent(false);
+                        break;
+                }
+                return false;
+            }
+        });
     }
 
     @OnClick({R.id.btn_add, R.id.btn_delete, R.id.btn_move, R.id.btn_change})
